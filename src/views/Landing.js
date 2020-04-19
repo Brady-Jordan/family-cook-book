8da762608b0f1aa046ce5c4ae12361dada487d96
Jordan Smithson <jordantroysmithson@gmail.com>
2020-04-18 22:30:15 -0600

    hero section build. not responsive yet.

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from '../components/header';
+import heroImg from '../assets/landing_hero_spices.jpg'
+import heroPotSVG from '../assets/landing_pot.png'
 import '../styles/views/landing.css'
 
 const Landing = () => {
@@ -7,14 +9,19 @@ const Landing = () => {
     return(
         <div>
             <Header/>
-            <div className="row">
-                <div className="col-6">
-                    <img src="" alt="" className="img-fluid"/>
+            <div className="container-fluid  w-100">
+            <div className="row p-0 ">
+                <div className="col-6 p-0 ">
+                    <img src={heroImg} alt="" className="img img-fluid p-0 m-0"/>
                 </div>
-                <div className="col-6">
-
+                <div className="col-6 p-5 potSection d-flex flex-column align-items-center justify-content-around">
+                    <img src={heroPotSVG} alt="" className='img img-fluid p-5 m-0'/>
+                    <button className="btn btn-primary ">Join the family!</button>
                 </div>
             </div>
+
+            </div>
+            
         </div>
     )
 }
