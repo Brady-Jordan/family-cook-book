5a261dfafb78494ac41e0b503c0f44b4737a181d
Jordan Smithson <jordantroysmithson@gmail.com>
2020-04-18 19:32:25 -0600

    added new blue scheme colors. created and styled header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import '../Styles/Components/header.css'
 
 const Header = () => {
 
     return(
-        <div>
-            <h1>header</h1>
+        <div className='header p-3'>
+            <div className="row d-flex justify-content-end">
+                <div className="col-6 d-flex justify-content-between align-items-center">
+                    <a href=""><p className="navLink">Home</p></a>
+                    <a href=""><p className="navLink">Recipes</p></a>
+                    <a href=""><p className="navLink">About</p></a>
+                    <a href=""><p className="navLink">Sign In</p></a>
+                    <div className="input-group w-50">
+                        <input type="text" class="form-control navSearch"/>
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fal fa-search"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
